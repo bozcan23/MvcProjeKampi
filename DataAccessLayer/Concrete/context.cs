ce96e3a13aa56d749ef0984c8418f39ec7028b34
bozcan23 <75613947+bozcan23@users.noreply.github.com>
2021-04-27 02:46:21 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Concrete/context.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class context:DbContext //aşağıda tanımlayacağımız propertiler tablo ismi olarak sql de karşımıza çıkacak.DATAaccess layer içine entity framework kurulmalı
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts{ get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
